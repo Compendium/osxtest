40f0d1f34e2f50efb543999a951638161402849d
Alexander <compendium@cirno.local>
2014-01-16 22:04:35 +0100

    Initial Commit

---
+++ osxtest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  osxtest
+//
+//  Created by Alexander on 16/01/14.
+//  Copyright (c) 2014 Alexander. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
