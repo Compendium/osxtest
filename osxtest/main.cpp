e003312efbcb26da6f7569c41f6715fe1f39b349
Alexander <compendium@diezauberervonoz.org>
2014-02-04 22:09:50 +0100

    added .gitignore file for xcode stuffs, added vbo rendering. we're not deprecated anymore, i think!

--- osxtest/main.cpp
+++ osxtest/main.cpp
@@ -52,6 +52,7 @@ int main(int argc, const char * argv[])
     char * msgbuffer = new char[1024];
     int rlength = 0;
     
+    //fragment shader
     {
         std::ifstream file("resources/test.frag", std::ios::binary);
         if(file.good() == false) {
@@ -71,12 +72,19 @@ int main(int argc, const char * argv[])
         
         const char *c_str = buffer.data();
         glShaderSource(fragmentShaderGLPtr, 1, &c_str, NULL);
+        
+        glCompileShader(fragmentShaderGLPtr);
+        
+        int ecode;
+        glGetShaderiv(fragmentShaderGLPtr, GL_COMPILE_STATUS, &ecode);
+        if(ecode == GL_FALSE){
+            cout << "compiling fragment shader error:";
+            glGetShaderInfoLog(fragmentShaderGLPtr, 1024, &rlength, msgbuffer);
+            cout << msgbuffer << endl;
+        }
     }
     
-    glCompileShader(fragmentShaderGLPtr);
-    glGetShaderInfoLog(fragmentShaderGLPtr, 1024, &rlength, msgbuffer);
-    cout << msgbuffer << endl;
-    
+    //vertex shader
     {
         std::ifstream file("resources/test.vert", std::ios::binary);
         if(file.good() == false) {
@@ -96,63 +104,126 @@ int main(int argc, const char * argv[])
         
         const char *c_str = buffer.data();
         glShaderSource(vertexShaderGLPtr, 1, &c_str, NULL);
+        
+        glCompileShader(vertexShaderGLPtr);
+        
+        int ecode;
+        glGetShaderiv(vertexShaderGLPtr, GL_COMPILE_STATUS, &ecode);
+        if(ecode == GL_FALSE) {
+            cout << "compiling vertex shader error:";
+            glGetShaderInfoLog(vertexShaderGLPtr, 1024, &rlength, msgbuffer);
+            cout << msgbuffer << endl;
+        }
     }
-
-    glCompileShader(vertexShaderGLPtr);
-    glGetShaderInfoLog(vertexShaderGLPtr, 1024, &rlength, msgbuffer);
-    cout << msgbuffer << endl;
-
     
-    glAttachShader(shaderGLPtr, vertexShaderGLPtr);
-    glAttachShader(shaderGLPtr, fragmentShaderGLPtr);
-    glLinkProgram(shaderGLPtr);
+    //link both fragment and vertex shader into shaderprogram
+    {
+        glAttachShader(shaderGLPtr, vertexShaderGLPtr);
+        glAttachShader(shaderGLPtr, fragmentShaderGLPtr);
+        
+        int ecode;
+        
+        glLinkProgram(shaderGLPtr);
+        glGetProgramiv(shaderGLPtr, GL_LINK_STATUS, &ecode);
+        if(ecode == GL_FALSE) {
+            cout << "linking shaderprogram error:";
+            glGetProgramInfoLog(shaderGLPtr, 1024, &rlength, msgbuffer);
+            cout << msgbuffer << endl;
+        }
+        
+        glValidateProgram(shaderGLPtr);
+        glGetProgramiv(shaderGLPtr, GL_VALIDATE_STATUS, &ecode);
+        if(ecode == GL_FALSE) {
+            cout << "validating shaderprogram error:";
+            glGetProgramInfoLog(shaderGLPtr, 1024, &rlength, msgbuffer);
+            cout << msgbuffer << endl;
+        }
+    }
     
-    glGetProgramInfoLog(shaderGLPtr, 1024, &rlength, msgbuffer);
-    cout << msgbuffer << endl;
     
-    glValidateProgram(shaderGLPtr);
-    glGetProgramInfoLog(shaderGLPtr, 1024, &rlength, msgbuffer);
-    cout << msgbuffer << endl;
     
     glm::mat4 Projection = glm::perspectiveFov(90.0f, (float)pw, (float)ph, 1.0f, 1000.0f);
-    glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -100.0f));
+    glm::mat4 ViewTranslate = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -1.0f));
     glm::mat4 View = ViewTranslate;
     glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
     glm::mat4 MVP = Projection * View * Model;
     
     int uniform_matrix_mvp = glGetUniformLocation(shaderGLPtr, "umvp");
+    int uniform_deltatime = glGetUniformLocation(shaderGLPtr, "udt");
     
+    /* Create a variable to hold the VBO identifier */
+    GLuint triangleVBO;
     
+    const unsigned int shaderAttribute = glGetAttribLocation(shaderGLPtr, "attrib_vertexpos");
+    
+    const float NUM_OF_VERTICES_IN_DATA=3;
+    
+    /* Vertices of a triangle (counter-clockwise winding) */
+    float data[3][3] = {
+        {  0.0, 1.0, 0.0   },
+        { -1.0, -1.0, 0.0  },
+        {  1.0, -1.0, 0.0  }
+    };
+    
+    /* Create a new VBO and use the variable "triangleVBO" to store the VBO id */
+    glGenBuffers(1, &triangleVBO);
+    
+    /* Make the new VBO active */
+    glBindBuffer(GL_ARRAY_BUFFER, triangleVBO);
+    
+    /* Upload vertex data to the video device */
+    glBufferData(GL_ARRAY_BUFFER, NUM_OF_VERTICES_IN_DATA * 3 * sizeof(float), data, GL_STATIC_DRAW);
+    
+    /* Specify that our coordinate data is going into attribute shaderAttribute, and contains three floats per vertex */
+    glVertexAttribPointer(shaderAttribute, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    
+    /* Enable attribute index 0(shaderAttribute) as being used */
+    glEnableVertexAttribArray(shaderAttribute);
+    
+    /* Make the new VBO active. */
+    glBindBuffer(GL_ARRAY_BUFFER, triangleVBO);
+
     // ... can be used alongside SDL2.
     SDL_Event e;
     float x = 0.0, y = 30.0;
     bool keeprunning = true;
     
     while (keeprunning) {
-        //update loop
+        //update loop -- first work all the events
         while (SDL_PollEvent(&e) != 0) {
             if(e.type == SDL_KEYDOWN || e.type == SDL_QUIT)
                 keeprunning = false;
+        }//then do all the not-event stuff
+        {
+            int mousex, mousey;
+            SDL_GetMouseState(&mousex, &mousey);
+            int windowwidth, windowheight;
+            SDL_GetWindowSize(window, &windowwidth, &windowheight);
+            if(mousex < windowwidth * 0.1) {
+                MVP = glm::translate(MVP, glm::vec3(1.0f, 0.0f, 0.0f));
+            } else if (mousex > windowwidth - windowwidth * 0.1) {
+                MVP = glm::translate(MVP, glm::vec3(-1.0f, 0.0f, 0.0f));
+            }
+            if(mousey < windowheight * 0.08) {
+                MVP = glm::translate(MVP, glm::vec3(0.0f, -1.0f, 0.0f));
+            } else if (mousey > windowheight - windowheight * 0.08) {
+                MVP = glm::translate(MVP, glm::vec3(0.0f, 1.0f, 0.0f));
+            }
         }
         
         //render loop
         {            
             glUseProgram(shaderGLPtr);
             glUniformMatrix4fv(uniform_matrix_mvp, 1, GL_FALSE, glm::value_ptr(MVP));
+            glUniform1f(uniform_deltatime, SDL_GetTicks());
             
             glClearColor(0,0,0,1);          // Draw with OpenGL.
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            //glRotatef(10.0,0.0,0.0,1.0);
-            // Note that the glBegin() ... glEnd() OpenGL style used below is actually
-            // obsolete, but it will do for example purposes. For more information, see
-            // SDL_GL_GetProcAddress() or find an OpenGL extension loading library.
-            glBegin(GL_TRIANGLES);
             
-            glColor3f(1.0,0.0,0.0); glVertex2f(x, y+90.0);
-            glColor3f(0.0,1.0,0.0); glVertex2f(x+90.0, y-90.0);
-            glColor3f(0.0,0.0,1.0); glVertex2f(x-90.0, y-90.0);
-            glEnd();
-            
+            /* Actually draw the triangle, giving the number of vertices provided by invoke glDrawArrays
+             while telling that our data is a triangle and we want to draw 0-3 vertexes
+             */
+            glDrawArrays(GL_TRIANGLES, 0, 3);
             
             SDL_GL_SwapWindow(window);  // Swap the window/buffer to display the result.
             SDL_Delay(15);              // Pause briefly before moving on to the next cycle.
--- osxtest/resources/test.vert
+++ osxtest/resources/test.vert
@@ -1,10 +1,12 @@
 #version 120
 
 uniform mat4 umvp;
+uniform float udt;
+attribute vec3 attrib_vertexpos;
 varying vec4 frag_color;
 
 void main()
 {
-    gl_Position = umvp * gl_Vertex;
+    gl_Position = umvp * vec4(attrib_vertexpos.x, attrib_vertexpos.y * sin(udt*0.002 + attrib_vertexpos.x * 0.1), attrib_vertexpos.z, 1.0f);
     gl_FrontColor = gl_Color;
 }
